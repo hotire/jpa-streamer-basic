362956ad24f60f504c41b6a7db19429d87531b4e
hotire <gngh0101@gmail.com>
2021-08-22 15:25:25 +0900

    Add stream extension of JPAStreamer

---
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerRepositorySupport.kt
@@ -0,0 +1,10 @@
+package com.github.hotire.jpa.streamer.basic
+
+import com.speedment.jpastreamer.application.JPAStreamer
+
+inline fun <reified T> JPAStreamer.stream() = this.stream(T::class.java)
+
+open class JpaStreamerRepositorySupport<T>(private val jpaStreamer: JPAStreamer) {
+
+
+}
