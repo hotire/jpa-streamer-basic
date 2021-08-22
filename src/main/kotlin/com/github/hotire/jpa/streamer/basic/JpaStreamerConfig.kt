beff7e850cfffffabdedb083e9ff32d7e0de42ed
hotire <gngh0101@gmail.com>
2021-08-22 15:08:58 +0900

    Add JpaStreamerConfig

---
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerConfig.kt
@@ -0,0 +1,13 @@
+package com.github.hotire.jpa.streamer.basic
+
+import com.speedment.jpastreamer.application.JPAStreamer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManagerFactory
+
+@Configuration
+class JpaStreamerConfig {
+    
+    @Bean
+    fun jpaStreamer(entityManagerFactory: EntityManagerFactory): JPAStreamer = JPAStreamer.createJPAStreamerBuilder(entityManagerFactory).build()
+}
