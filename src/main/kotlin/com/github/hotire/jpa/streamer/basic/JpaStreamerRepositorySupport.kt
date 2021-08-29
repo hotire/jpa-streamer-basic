2a5023fd8dc726ae44c36626624288c1f186e4bf
hotire <gngh0101@gmail.com>
2021-08-29 15:22:32 +0900

    Add JpaStreamerRepositorySupport

--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerRepositorySupport.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerRepositorySupport.kt
@@ -1,7 +1,14 @@
 package com.github.hotire.jpa.streamer.basic
 
 import com.speedment.jpastreamer.application.JPAStreamer
+import org.springframework.core.ResolvableType
 
 inline fun <reified T> JPAStreamer.stream() = this.stream(T::class.java)
 
-open class JpaStreamerRepositorySupport<T>(private val jpaStreamer: JPAStreamer)
+interface JpaStreamerRepositorySupport<T> {
+    val jpaStreamer: JPAStreamer
+
+    val entityType: Class<*>
+        get() = ResolvableType.forClass(JpaStreamerRepositorySupport::class.java, this::class.java).generics[0]
+            .resolve()
+}
