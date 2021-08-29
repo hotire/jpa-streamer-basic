b84fa0ce9fcc40a018512c7d07c22b8555acd41f
hotire <gngh0101@gmail.com>
2021-08-30 00:01:52 +0900

    Remove unused code

--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerRepositorySupport.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/JpaStreamerRepositorySupport.kt
@@ -1,14 +1,13 @@
 package com.github.hotire.jpa.streamer.basic
 
 import com.speedment.jpastreamer.application.JPAStreamer
-import org.springframework.core.ResolvableType
 
 inline fun <reified T> JPAStreamer.stream() = this.stream(T::class.java)
 
 interface JpaStreamerRepositorySupport<T> {
     val jpaStreamer: JPAStreamer
 
-    val entityType: Class<*>
-        get() = ResolvableType.forClass(JpaStreamerRepositorySupport::class.java, this::class.java).generics[0]
-            .resolve()
+//    val entityType: Class<*>
+//        get() = ResolvableType.forClass(JpaStreamerRepositorySupport::class.java, this::class.java).generics[0]
+//            .resolve()
 }
