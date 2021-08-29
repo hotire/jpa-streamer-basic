27cafd8685433f02361d215849fc84994d8afff6
hotire <gngh0101@gmail.com>
2021-08-29 15:05:49 +0900

    Add CustomizedFilmRepositoryImpl

--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/ex/FilmRepository.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/ex/FilmRepository.kt
@@ -1,5 +1,10 @@
 package com.github.hotire.jpa.streamer.basic.ex
 
+import com.speedment.jpastreamer.application.JPAStreamer
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FilmRepository : JpaRepository<Film, Long>
+
+interface CustomizedFilmRepository
+
+class CustomizedFilmRepositoryImpl(private val jpaStreamer: JPAStreamer) : CustomizedFilmRepository
