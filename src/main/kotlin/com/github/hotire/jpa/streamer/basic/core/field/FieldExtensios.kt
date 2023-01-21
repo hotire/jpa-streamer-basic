874919708d069e7e75ee9258d73f3908d05c841f
hotire <gngh0101@gmail.com>
2023-01-21 12:56:51 +0900

    Add FieldExtensions

---
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/core/field/FieldExtensios.kt
@@ -0,0 +1,12 @@
+package com.github.hotire.jpa.streamer.basic.core.field
+
+import com.speedment.jpastreamer.field.LongField
+import kotlin.reflect.KProperty1
+
+/**
+ * @see com.speedment.jpastreamer.field.Field
+ */
+
+inline fun <reified T : Any> KProperty1<T, Long>.longField(unique: Boolean): LongField<T> {
+    return LongField.create(T::class.java, name, this, unique)
+}
