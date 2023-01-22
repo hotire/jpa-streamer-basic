20512ebb45cec5af8539d0720b1b5025827c349f
hotire <gngh0101@gmail.com>
2023-01-22 23:34:35 +0900

    Use longField extensions

--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/core/field/FieldExtensios.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/core/field/FieldExtensios.kt
@@ -7,6 +7,6 @@ import kotlin.reflect.KProperty1
  * @see com.speedment.jpastreamer.field.Field
  */
 
-inline fun <reified T : Any> KProperty1<T, Long>.longField(unique: Boolean): LongField<T> {
+inline fun <reified T : Any> KProperty1<T, Long>.longField(unique: Boolean = false): LongField<T> {
     return LongField.create(T::class.java, name, this, unique)
 }
--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/person/PersonService.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/person/PersonService.kt
@@ -1,5 +1,6 @@
 package com.github.hotire.jpa.streamer.basic.person
 
+import com.github.hotire.jpa.streamer.basic.core.field.longField
 import com.speedment.jpastreamer.application.JPAStreamer
 import java.util.stream.Collectors
 import org.springframework.stereotype.Service
@@ -9,7 +10,8 @@ class PersonService(private val jpaStreamer: JPAStreamer) {
 
     fun findAllByIdIn(ids: List<Long>) {
         jpaStreamer.stream(Person::class.java)
-            .filter(`Person$`.id.`in`(ids))
+            .filter(Person::id.longField(true).`in`(ids))
+//            .filter(`Person$`.id.`in`(ids))
             .collect(Collectors.toList())
     }
 }
