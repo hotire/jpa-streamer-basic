67ecadda0ce9f3bdff68503a91b16497af875069
hotire <gngh0101@gmail.com>
2023-02-01 02:58:49 +0900

    Add enumField

--- src/main/kotlin/com/github/hotire/jpa/streamer/basic/core/field/FieldExtensios.kt
+++ src/main/kotlin/com/github/hotire/jpa/streamer/basic/core/field/FieldExtensios.kt
@@ -1,6 +1,7 @@
 package com.github.hotire.jpa.streamer.basic.core.field
 
 import com.speedment.jpastreamer.field.ComparableField
+import com.speedment.jpastreamer.field.EnumField
 import com.speedment.jpastreamer.field.LongField
 import kotlin.reflect.KProperty1
 
@@ -14,4 +15,8 @@ inline fun <reified T : Any> KProperty1<T, Long>.longField(unique: Boolean = fal
 
 inline fun <reified T : Any> KProperty1<T, Long?>.longNullableField(unique: Boolean = false): ComparableField<T, Long> {
     return ComparableField.create(T::class.java, name, { this?.call() }, unique)
+}
+
+inline fun <reified T : Any, reified E : Enum<E>> KProperty1<T, E>.enumField(): EnumField<T, E> {
+    return EnumField.create(T::class.java, name, this, E::class.java)
 }
